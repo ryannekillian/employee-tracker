2df2b84ac7f8483f345886e8acb0c18b6587c1cb
ryannekillian <ryanne.killian@gmail.com>
2020-08-09 11:47:13 -0500

    added dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require("mysql2");
-
+console.log(process.env.DB_PASSWORD)
 const connection = mysql.createConnection({
   host: "localhost",
   // Your username
   user: "root",
   // Your password
-  password: "",
+  password: process.env.DB_PASSWORD,
   database: "employees"
 });
 
